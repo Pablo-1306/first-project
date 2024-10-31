aa2791b8c2ea4f00c6702d905481c0737c651807
GaelAlburo <gael.a24@outlook.com>
2024-10-31 14:09:08 -0600

    feat(footer): add responsive footer

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -1,163 +1,174 @@
 "use client"
-import { AppBar, Box, Button, Divider, Switch, TextField, Toolbar, Typography } from "@mui/material";
+
+import { Box, Container, Typography, IconButton, useTheme, TextField } from "@mui/material";
 import Grid from "@mui/material/Grid2";
 import Link from "next/link";
-import Image from "next/image";
-import { theme } from "../styles/global-theme";
-import { AccountCircle } from "@mui/icons-material";
+import TwitterIcon from '@mui/icons-material/Twitter';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 export default function FooterGlobal() {
+    const theme = useTheme();
 
-  return (
-    <AppBar position='static' sx={{backgroundColor:theme.palette.footer.main}}>
-      <Toolbar >
-        <Box >
-          <Grid container spacing={30} width={'90%'} ml={'auto'} mr={'auto'}>
-                <Grid size={{ xs: 12, sm: 6, md: 2 }} sx={{ mb: 4, mt:4, display:'flex'}}>
-                    <Box sx={{display:'flex'}}>
-                        {/* <Image
-                            src="/logo.png"
-                            alt="Our Logo"
-                            height={40}
-                            width={40}
-                            priority
-                        /> */}
-                        <Typography
-                        variant="h5"
-                        noWrap
-                        sx={{
-                            display: { xs: "none", md: "flex" },
-                            fontFamily: "Grenze Gotisch",
-                            fontWeight: 700,
-                            letterSpacing: ".05rem",
-                            color: theme.palette.primary.main,
-                            textDecoration: "none",
-                            ml: 2,
-                            color: 'white'
-                        }}
-                        >
-                            EduTrack
-                        </Typography>
-                    </Box>
-                </Grid>
-                <Grid size={{ xs: 12, sm: 6, md: 2 }} sx={{ mb: 4,mt:4, display:'flex'}}>
-                    <Box>
-                        <Typography
-                        variant="h5"
-                        color="white"
-                        fontWeight={'bold'}
-                        >
-                            Company
-                        </Typography>
-                    <Box>
-                        <Typography
-                        variant="h7"
-                        color="white"
-                        component={Link}
-                        href="/about-us"
-                        >
-                            Brand
-                        </Typography>
-                    </Box>
-                    <Box>
-                        <Typography
-                        variant="h7"
-                        color="white"
-                        component={Link}
-                        href="/about-us"
-                        >
-                            Terms & Conditions
-                        </Typography>
-                    </Box>
-                    </Box>
-                </Grid>
-                <Grid size={{ xs: 12, sm: 6, md: 2 }} sx={{ mb: 4,mt:4, display:'flex' }}>
-                    <Box>
-                        <Typography
-                        variant="h5"
-                        color="white"
-                        fontWeight={'bold'}
-                        >
-                            Resources
-                        </Typography>
-                        <Box>
-                        <Typography
-                        variant="h7"
-                        color="white"
-                        component={Link}
-                        href="/qa"
-                        >
-                            Comunnity Q&A
-                        </Typography>
-                        </Box>
-                        <Box>
-                        <Typography
-                        variant="h7"
-                        color="white"
-                        component={Link}
-                        href="/qa"
-                        >
-                            Shipping & Changes
-                        </Typography>
-                        </Box>
-                    </Box>
-                </Grid>
-                <Grid size={{ xs: 12, sm: 6, md: 2 }} sx={{ mb: 4,mt:4, display:'flex' }}>
-                    <Box>
-                        <Typography
-                        variant="h5"
-                        color="white"
-                        fontWeight={'bold'}
-                        >
-                            Contact
-                        </Typography>
-                        <Typography
-                        variant="h7"
-                        color="white"
-                        component={Link}
-                        href="/support"
-                        >
-                            Support
-                        </Typography>
-                    </Box>
-                </Grid>
-                <Grid size={{ xs: 12, sm: 6, md: 4 }} sx={{ mb: 4,mt:4, display:'flex' }}>
-                    <Box>
-                        <Typography
-                        variant="h6"
-                        color="white"
-                        >
-                            Subscribe to our newsletter
-                        </Typography>
-                        <Box sx={{ display: 'flex', alignItems: 'flex-end'}}>
-                            <AccountCircle sx={{ color: 'white', mr: 1, my: 0.5 }} />
-                            <TextField id="input-with-sx" label="Email" variant="standard" sx={{color:'white'}}/>
-                            <Button sx={{ml:1, backgroundColor: theme.palette.primary.main}} color="white">Subscribe</Button>
-                        </Box>
-                    </Box>
-                </Grid>
-            </Grid>
-        </Box>
-        
-      </Toolbar>
-      <Divider />
-      <Box
-          sx={{
-            justifyContent: "center",
-            display: "flex",
-            mb: 2,
-            mt: 1
-          }}
+    return (
+        <Container maxWidth='100vw' disableGutters
+            sx={{
+                backgroundColor: theme.palette.secondary.main,
+                border: '1px solid white'
+            }}
         >
-          <Typography variant="caption" color="white" >
-            Copyright ©2024. PR_EduTrack Cookies Preferences
-          </Typography>
-          <Switch
-            checked={true}
-            // onChange={handleChange}
-            inputProps={{ 'aria-label': 'controlled' }}
-            />
-        </Box>
-    </AppBar>
-  );
+            <Container maxWidth='md'>
+                <Grid container >
+                    
+                    {/*LEFT COLUMN */}
+                    <Grid size={{xs: 12, md: 6}} color={theme.palette.primary.main}
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center'
+                        }}
+                    >
+                        <Box
+                            sx={{
+                                mt: 6,
+                                mb: {
+                                    xs: 2,
+                                    md: 6
+                                },
+                                textAlign: {
+                                    xs: 'center',
+                                    md: 'left'
+                                }
+                            }}
+                        >
+                            <Box display='flex'
+                                sx={{
+                                    mb: 3,
+                                    display: 'flex',
+                                    justifyContent: {
+                                        xs: 'center',
+                                        md: 'left'
+                                    }
+                                }}
+                            >
+                                <Typography component={Link} href='/about' color={theme.palette.primary.main}
+                                    sx={{textDecoration: 'none'}}
+                                >
+                                    BRAND
+                                </Typography>
+                                <Typography component={Link} href='/contact' color={theme.palette.primary.main}
+                                    sx={{ml: 5, textDecoration: 'none'}}
+                                >
+                                    CONTACT
+                                </Typography>
+                            </Box>
+                            <Typography component={Link} href='/shipment' color={theme.palette.primary.main}
+                                sx={{mb: 3, textDecoration: 'none', display: 'block'}}
+                            >
+                                SHIPPING AND CHANGES
+                            </Typography>
+                            <Typography component={Link} href='/faq' color={theme.palette.primary.main}
+                                sx={{mb: 3, textDecoration: 'none', display: 'block'}}
+                            >
+                                FREQUENT ASKED QUESTIONS
+                            </Typography >
+                            <Typography component={Link} href='/terms' color={theme.palette.primary.main}
+                                sx={{mb: 3, textDecoration: 'none', display: 'block'}}
+                            >
+                                TERMS AND CONDITIONS
+                            </Typography>
+                        </Box>
+                    </Grid>
+                    
+                    {/*RIGHT COLUMN */}
+                    <Grid size={{xs: 12, md: 6}} color={theme.palette.primary.main}
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: {
+                                xs: 'center',
+                                md: 'end'
+                            }
+                        }}
+                    >
+                        <Box
+                            sx={{
+                                my: {
+                                    xs: 4,
+                                    md: 6
+                                }
+                            }}
+                        >
+                            <Typography
+                                variant="h5"
+                                noWrap
+                                component={Link}
+                                href="/"
+                                sx={{
+                                    fontFamily: "Grenze Gotisch",
+                                    fontWeight: 600,
+                                    letterSpacing: ".05rem",
+                                    color: "inherit",
+                                    textDecoration: "none",
+                                }}
+                            >
+                                TiendaRopa
+                            </Typography>
+
+                            <Box sx={{mt: 1.5, width: 112}}>
+                                <IconButton sx={{color: theme.palette.primary.main, p: 0, mr: 2, ml: 1}}>
+                                    <TwitterIcon/>
+                                </IconButton>
+                                <IconButton sx={{color: theme.palette.primary.main, p: 0, mr: 2}}>
+                                    <InstagramIcon />
+                                </IconButton>
+                                <IconButton sx={{color: theme.palette.primary.main, p: 0}}>
+                                    <FacebookIcon />
+                                </IconButton>
+                            </Box>
+                        </Box>
+
+                        <Box
+                            sx={{
+                                mb: {
+                                    xs: 6,
+                                    md: 0
+                                }
+                            }}
+                        >
+                            <Typography variant="subtitle2"
+                                sx={{
+                                    fontSize: 10,
+                                    textAlign: {
+                                        md: 'right',
+                                        xs: 'center'
+                                    }
+                                }}
+                            >
+                                Receive news from TiendaRopa
+                            </Typography>
+                            <TextField 
+                                placeholder="Your email"
+                                variant="standard"
+                                size="small"
+                                
+                                sx={{
+                                    border: '1px solid white',
+                                    '& .MuiInputBase-input': { //Placeholder text
+                                        color: 'white',
+                                        textAlign: {
+                                            xs: 'center',
+                                            md: 'right'
+                                        } 
+                                    },
+                                }}
+                            />
+                        </Box>
+                    </Grid>
+
+                </Grid>
+
+            </Container>
+        </Container>
+    );
 }

69fa4ec0b53a374dc17e2d11a94f3178697e3d73
GaelAlburo <gael.a24@outlook.com>
2024-11-04 20:06:21 -0600

    feat(categories): add a dynamic route for different categories

--- app/categories/women/page.jsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import CategoryProducts from '../../components/category-product-manager';
-
-export default function WomenPage() {
-  return <CategoryProducts categoryId="women" />;
-}

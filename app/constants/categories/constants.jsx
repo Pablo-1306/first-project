f65a972a25f4dbee49cec469afd411b7d3897c13
GaelAlburo <gael.a24@outlook.com>
2024-11-04 20:13:18 -0600

    feat(categories): change categories and products constants so they work with categories context

--- app/constants/categories/constants.jsx
+++ app/constants/categories/constants.jsx
@@ -1,13 +1,16 @@
 export const initialCategories = [
     {
+      id: '1',
       label: 'Women',
-      path: '/categories//women'
+      path: '/categories/women'
     },
     {
+      id: '2',
       label: 'Men',
       path: '/categories/men'
     },
     {
+      id: '3',
       label: 'Child',
       path: '/categories/child'
     }
--- app/constants/products/constants.jsx
+++ app/constants/products/constants.jsx
@@ -5,56 +5,56 @@ export const initialProducts = [
       name: 'Blue Shirt',
       price: '$1,000.00 MXN',
       image: '/shirt-test.jpeg',
-      category: 'women'  
+      category: 'Women'  
     },
     {
       id: '2',
       name: 'Pink Sweater',
       price: '$2,000.00 MXN',
       image: '/shirt-test.jpeg',
-      category: 'men'
+      category: 'Men'
     },
     {
       id: '3',
       name: 'Black Jacket',
       price: '$1,500.00 MXN',
       image: '/shirt-test.jpeg',
-      category: 'child'
+      category: 'Child'
     },
     {
       id: '4',
       name: 'Green Skirt',
       price: '$1,800.00 MXN',
       image: '/shirt-test.jpeg',
-      category: 'men'
+      category: 'Men'
     },
     {
       id: '5',
       name: 'Purple Pants',
       price: '$1,500.00 MXN',
       image: '/shirt-test.jpeg',
-      category: 'women'
+      category: 'Women'
     },
     {
       id: '6',
       name: 'Yellow Dress',
       price: '$1,800.00 MXN',
       image: '/shirt-test.jpeg',
-      category: 'child'
+      category: 'Child'
     },
     {
         id: '7',
         name: 'Yellow Dress',
         price: '$1,800.00 MXN',
         image: '/shirt-test.jpeg',
-        category: 'women'
+        category: 'Women'
       },
       {
         id: '8',
         name: 'Yellow Dress',
         price: '$1,800.00 MXN',
         image: '/shirt-test.jpeg',
-        category: 'men'
+        category: 'Men'
       }
     
   ]; 

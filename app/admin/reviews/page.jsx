1f43e0b7ae7837abc2392e91de611a8291dd60e1
GaelAlburo <gael.a24@outlook.com>
2024-11-26 01:42:46 -0600

    feat(reviews): add backend support for the reviews_API

--- app/admin/reviews/page.jsx
+++ app/admin/reviews/page.jsx
@@ -13,6 +13,9 @@ import Grid from "@mui/material/Grid2";
 import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
 import { useReviews } from "@/app/contexts/ReviewContext";
 import Link from "next/link";
+import Alerts from "../../components/alerts";
+import { useState } from "react";
+import axios from "axios";
 
 // Reviews page accessed at /admin/reviews
 export default function Reviews() {
@@ -20,6 +23,34 @@ export default function Reviews() {
 
   //Give us access to the reviews and the deleteReview function from the ReviewContext
   const { reviews, deleteReview } = useReviews();
+
+  // State to open or close the alert
+  const [openAlert, setOpenAlert] = useState(false);
+
+  // Alert state to show messages to the user when adding a review or product to the cart
+  const [alert, setAlert] = useState({
+    message: "",
+    severity: "",
+  });
+
+
+  const handleDelete = async (review_id) => {
+    try {
+      const response = await axios.delete(`http://localhost:8000/api/v1/reviews/${review_id}`);
+      deleteReview(review_id);
+      setAlert({
+        message: "Review deleted successfully",
+        severity: "success",
+      });
+    } catch (error) {
+      setAlert({
+        message: "Failed to delete the review",
+        severity: "error",
+      });
+      console.error("Error deleting review: ", error);
+    }
+    setOpenAlert(true); // Open the alert
+  }
 
   return (
     <Container maxWidth="lg">
@@ -82,7 +113,7 @@ export default function Reviews() {
         <Typography variant="h3">Reviews</Typography>
 
         {reviews.map((review) => (
-          <Box key={review.id}>
+          <Box key={review._id}>
             <Grid container sx={{ mt: 6, mb: 3 }}>
               <Grid size={{ md: 10 }} sx={{ textAlign: "left" }}>
                 <Typography variant="body1" sx={{ mb: 1 }}>
@@ -108,7 +139,7 @@ export default function Reviews() {
                 <IconButton
                   aria-label="delete"
                   size="large"
-                  onClick={() => deleteReview(review.id)}
+                  onClick={() => handleDelete(review._id)}
                 >
                   <DeleteOutlinedIcon
                     fontSize="inherit"
@@ -122,6 +153,7 @@ export default function Reviews() {
           </Box>
         ))}
       </Container>
+      <Alerts open={openAlert} setOpen={setOpenAlert} alert={alert} />
     </Container>
   );
 }
